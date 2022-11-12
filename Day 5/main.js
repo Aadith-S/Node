f67b39edb25a8457bdc1416802cde9cdefbf7d83
Aadith S <aadith.s@experionglobal.local>
2022-11-12 12:13:25 +0530

    D6

--- Day 5/main.js
+++ Day 5/main.js
@@ -1,14 +1,34 @@
 const fs = require("fs");
-var count = 1;
 const readline = require("readline-sync");
 const { json } = require("stream/consumers");
 var record = {};
-
+var count = 1;
+if(fs.existsSync("PassengerList.json"))
+    {
+        record = JSON.parse(fs.readFileSync("PassengerList.json","utf8"));
+        if(record != "")
+        {
+        count = Object.keys(record).pop();
+        count++;
+        }
+    }
+else{
+    fs.writeFileSync("PassengerList.json","");
+}
 function addPassenger(id){
     let name = readline.question("Input Name:");
     let age = readline.question("Input Age:");
     let sex = readline.question("Input Sex:");
-    let pno = readline.question("Input Phone Number:")
+    let pno = [];
+    let mail = readline.question("Input Email:");
+    while(true){
+    let pnoe = readline.question("Input Phone Numbers:");
+    pno.push(pnoe);
+    let y = readline.question("Want to enter more(Y/N):");
+    if(y == "n" || y == "N"){
+        break;
+    }
+    }
     if(fs.readFileSync("PassengerList.json").length != 0){
     record = JSON.parse(fs.readFileSync("PassengerList.json","utf-8"));
     }
@@ -17,20 +37,48 @@ function addPassenger(id){
         Name : name,
         Age : age,
         Sex : sex,
+        Email : mail,
         Phone : pno
     }
     record[id] = passenger;
     fs.writeFileSync("PassengerList.json",JSON.stringify(record));
     count++;
 }
-
-function getPassenger(id){
-    record = JSON.parse(fs.readFileSync("PassengerList.json","utf-8"));
-    if(record[id]!=undefined){
-        console.log(record[id]);
-    }
-    else{
-        console.log("not Found");
+async function getData(){
+    return fs.readFile("PassengerList.json","utf8",(err,data)=>{
+            if(err){
+            return "No Data";
+            }
+            return JSON.parse(data);
+        });
+}
+function getPassenger(){
+    record =JSON.parse(fs.readFileSync("PassengerList.json","utf8"));
+    console.log("1 . Search by ID");
+    console.log("2 . Search by email");
+    let option = parseInt(readline.question("Enter option:"));
+    switch(option)
+    {
+        case 1 : let id = parseInt(readline.question("Enter ID to Search:"));
+            if(record[id]!=undefined){
+            console.log(record[id]);
+        }
+        else{
+            console.log("not Found");
+        }
+        break;
+        case 2 : let mail = readline.question("Enter Email to Search:");
+        for(let i = 1; i< count ; i++){
+            if(record[i].Email == mail){
+                console.log(record[i]);
+                break;
+            }
+            else{
+                console.log("Not found");
+            }
+        }
+        break;
+        default:console.log("Wrong option");
     }
 }
 
@@ -38,10 +86,11 @@ function updatePassenger(id){
     var record = JSON.parse(fs.readFileSync("PassengerList.json","utf-8"));
     console.log();
     console.log("UPDATE PASSENGER");
-    console.log("1 . Update Name");
-    console.log("2 . Update Age");
-    console.log("3 . Update Sex");
-    console.log("4 . Update Phone Number");
+    console.log("1 . Update Name:");
+    console.log("2 . Update Age;");
+    console.log("3 . Update Sex:");
+    console.log("4 . Update Phone Number:");
+    console.log("4 . Update Email:");
     console.log("5 . Cancel Update");
 
     let a = readline.question("Select an Option :")
@@ -52,7 +101,7 @@ function updatePassenger(id){
         break;
         case "3" : record[id].Sex = readline.question("Enter Sex:");
         break;
-        case "4" : record[id].Phone = parseInt(readline.question("Enter Phone Number:"));
+        case "4" : changephn(id);
         break;
         case "5" : break;
         default : console.log("Wrong Option!!")
@@ -62,10 +111,32 @@ function updatePassenger(id){
 }
 
 function deletePassenger(id){
-    var record = JSON.parse(fs.readFileSync("PassengerList.json","utf-8"));
+    record = JSON.parse(fs.readFileSync("PassengerList.json","utf-8"));
     delete record[id];
     fs.writeFileSync("PassengerList.json",JSON.stringify(record));
 }
+
+function changephn(id){
+    for(let i = 0 ;i<record[id].Phone.length;i++){
+    let phn = parseInt(readline.question("Edit Phone Number"+(i+1)+":"));
+    record[id].Phone[i] = phn == "" ? record[id].Phone[i]:phn;
+}
+}
+
+function alluser(){
+    record = JSON.parse(fs.readFileSync("PassengerList.json","utf-8"));
+    for(let i = 1 ; i< count ; i++){
+        console.log();
+        console.log("Passenger "+i)
+        console.log("ID:"+record[i].ID);
+        console.log("Name:"+record[i].Name);
+        console.log("Age:"+record[i].Age);
+        console.log("Sex:"+record[i].Sex);
+        console.log("Email:"+record[i].Email);
+        console.log("Phone Numbers:"+record[i].Phone);
+    }
+}
+
 function main(){ 
     while(true){
         console.log();
@@ -74,14 +145,14 @@ function main(){
         console.log("2 . Search User");
         console.log("3 . Update User");
         console.log("4 . Delete User");
-        console.log("5 . Exit");
+        console.log("5 . Display All Users");
+        console.log("6 . Exit");
 
         let a = readline.question("Select an Option :")
         switch(a){
             case "1" : addPassenger(count);
             break;
-            case "2" : let opt1 = parseInt(readline.question("Enter ID to Search:"));
-            getPassenger(opt1);
+            case "2" :  getPassenger();
             break;
             case "3" : let opt2 = parseInt(readline.question("Enter ID to Update:"));
             updatePassenger(opt2);
@@ -89,7 +160,9 @@ function main(){
             case "4" : let opt3 = parseInt(readline.question("Enter ID to Delete:"));
             deletePassenger(opt3);
             break;
-            case "5" :  process.exit(0);
+            case "5" :  alluser();
+            break;
+            case "6" :  process.exit(0);
             default : console.log("Wrong Option!!")
             break;
         }
